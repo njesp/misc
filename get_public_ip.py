35ab4d29b5f3c2d01ab6e1cd1eb416c6bea499eb
Niels Jespersen <niels@jespersen.info>
2018-08-12 19:57:10 +0200

    Small changes

--- get_public_ip.py
+++ get_public_ip.py
@@ -1,5 +1,8 @@
 
 import json
 import requests
+import datetime
 ip = json.loads(requests.get('https://api.ipify.org?format=json').text)['ip']
-print(ip)
+with open("c:/temp/ip.txt", "a") as myfile:
+    myfile.write(datetime.datetime.today().strftime(
+        '%Y-%m-%d %H:%M:%S') + ' ' + ip + '\n')
