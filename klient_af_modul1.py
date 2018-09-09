00c9658bf0ab632b80fa71ac7f99447e87b6593e
Niels Jespersen <niels@jespersen.info>
2018-09-09 19:19:19 +0200

    Jaja, hermed commit msg

--- klient_af_modul1.py
+++ klient_af_modul1.py
@@ -1,2 +1,6 @@
 import modul1 as m
-print ('I klienten ' + str(m.f(22)))
+import sys
+print('I klienten ' + str(m.f(22)))
+print(sys.version)
+print(sys.version_info)
+assert sys.version_info >= (3, 7)
